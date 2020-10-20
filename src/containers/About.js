9d49489f082b2358dce7454f95576c38fbe5df2b
daniiyang <lestreng.begi@gmail.com>
2020-10-20 09:13:15 +0300

    - add map iter

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import data from "../data";
 
 
 const About = () => (
@@ -7,50 +8,22 @@ const About = () => (
             О нас
         </h2>
         <div className="about-container">
-            <div className="about-post0">
-                <img src="https://nvrsk-expert.ru/_nuxt/430355fa3be1c2ae8bc14d6c63fd7a7f.svg" className="about-img1"/>
-                <div className="about-description-container">
-                    <h3>
-                        Богатый опыт работы с 2012 года
-                    </h3>
-                    <p className="h-s">
-                        Гарантируем индивидуальный подход к каждому обращению, конфиденциальность и сопровождение
-                    </p>
-                </div>
-            </div>
-            <div className="about-post1">
-                <div className="about-description-container">
-                    <h3>
-                        Зона работы в работе
-                    </h3>
-                    <p className="h-s">
-                        Осуществляем экспертизы в Новороссийске, Анапе, Геленджике - по всему Краснодарскому краю страны
-                    </p>
-                </div>
-                <img src="https://nvrsk-expert.ru/_nuxt/8bdedb8afe1bab56daf3d408f776140d.svg" className="about-img"/>
-            </div>
-            <div className="about-post0">
-                <img src="https://nvrsk-expert.ru/_nuxt/ebde157036a0d13e41c73b71c13ff55e.svg" className="about-img"/>
-                <div className="about-description-container">
-                    <h3>
-                        Написали более 1000 экспертных заключений
-                    </h3>
-                    <p className="h-s">
-                        Квалификационные аттестаты по оценке
-                    </p>
-                </div>
-            </div>
-            <div className="about-post1">
-                <div className="about-description-container">
-                    <h3>
-                        Осуществляем более чем 35 видов экспертиз
-                    </h3>
-                    <p className="h-s">
-                        Контроль качества экспертных исследований на каждом этапе
-                    </p>
-                </div>
-                <img src="https://nvrsk-expert.ru/_nuxt/c41659872c5ef6c963658f44ca0a8500.svg" className="about-img"/>
-            </div>
+
+            {
+                data[0].about.map((about) =>
+                    <div className="about-post1">
+                        <img src={about.image} className="about-img1"/>
+                        <div className="about-description-container">
+                            <h3>
+                                {about.title}
+                            </h3>
+                            <p className="h-s">
+                                {about.description}
+                            </p>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     </div>
 )
